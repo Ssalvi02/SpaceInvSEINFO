53333911ad44b6a18199cde43ed6204c1cae6454
Caio Ducati Castilho <caiodcastilho@hotmail.com>
2023-04-17 22:55:22 -0300

    fix menu selector

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,7 +7,7 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField] private GameObject selector;
     private int pos_id = 0;
-    private Vector3[] positions = { new Vector3(120, 97, 0f), new Vector3(120, 67, 0f), new Vector3(120, 35, 0f) };
+    private Vector3[] positions = { new Vector3(600, 475, 0f), new Vector3(600, 325, 0f), new Vector3(600, 175, 0f) };
     void Start()
     {
         selector = GameObject.Find("Canvas/Selector");
